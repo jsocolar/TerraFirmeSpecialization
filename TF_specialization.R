a48eeff06048f80dca56fcf2a9b8c8a2a61af477
Jacob Socolar <jacob@Jacobs-MBP.domain>
2018-09-20 16:10:14 -0500

    progress

--- TF_specialization.R
+++ TF_specialization.R
@@ -1,3 +1,4 @@
+##### Extract Loreto observations from eBird basic dataset #####
 library(auk)
 setwd("/Users/TingleyLab/Desktop/useful_datasets/eBird/ebd_relFeb-2018")
 f_in <- "ebd_relFeb-2018.txt"
@@ -16,11 +17,28 @@ setwd(paste0("/Users/", User, "/Dropbox/Work/TerraFirmeSpecialization/"))
 
 save(Loreto_ebd, file="Loreto_ebd.Rdata")
 
-
+##### Load Loreto observations from eBird and create presence-absence dataframe #####
 User = "Jacob"
-Tind <- c("Crypturellus_variegatus", "Pharomachrus_pavoninus", "Monasa_morphoeus", 
-          "Capito_auratus", "Synallaxis_rutilans", "Sclerurus_caudacutus", "Myrmoborus_myotherinus",
-          "Schisticichla_leucostigma", "Formicarius_colma", "Myrmothera_campanisona", 
-          "Liosceles_thoracicus", "Schiffornis_turdina", "Cnipodectes_subbrunneus", 
-          "Tunchiornis_ochraceiceps")
+setwd(paste0("/Users/", User, "/Dropbox/Work/TerraFirmeSpecialization/"))
+load("Loreto_ebd.Rdata")
+
+Loreto_ebd <- Loreto_ebd[which(Loreto_ebd$approved == T), ]
+
+events <- unique(Loreto_ebd$sampling_event_identifier)
+species <- unique(Loreto_ebd$scientific_name)
+
+Loreto_pa <- matrix(data=0, nrow=length(events), ncol=length(species))
+
+# Below should finish in well under a minute
+for(i in 1:nrow(Loreto_ebd)){
+  Loreto_pa[which(events == Loreto_ebd$sampling_event_identifier[i]), 
+            which(species == Loreto_ebd$scientific_name[i])] <- 1
+}
+
+
+Tind <- c("Crypturellus variegatus", "Pharomachrus pavoninus", "Monasa morphoeus", 
+          "Capito auratus", "Synallaxis rutilans", "Sclerurus caudacutus", "Myrmoborus myotherinus",
+          "Schisticichla leucostigma", "Formicarius colma", "Myrmothera campanisona", 
+          "Liosceles thoracicus", "Schiffornis turdina", "Cnipodectes subbrunneus", 
+          "Tunchiornis ochraceiceps")
 Vind <- c("Crypturellus_undulatus", "Opisthocomus_hoatzin", "Nyctiprogne_leucopygia", "Capito_aurovirens")
